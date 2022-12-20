65b33485e2071be96cc8076f02d65290bc1c331a
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-12-20 22:58:59 +0300

    upload Harmonic Numbers

--- HarmonicNumbers/src/Main.java
+++ HarmonicNumbers/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number, i;
-        double sum = 1;
+        double sum = 0;
         System.out.print("Enter the harmonic number (n) : ");
         number = scanner.nextInt();
 
